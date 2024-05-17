b4926aeacd9da735bbc074d9ce81a2167ce53c6a
root <root@DESKTOP-40K4BTE>
2024-05-17 12:18:59 +0300

    ..

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,23 +16,30 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.split('.')[0]
-    release_path = f'/data/web_static/releases/{folder_name}'
-    tmp_path = f'/tmp/{file_name}'
-    current_path = f'/data/web_static/current'
-    for host in env.hosts:
-        put(archive_path, '/tmp/')
-        result = c.run(f'tar -xzvf {tmp_path} -C {release_path}', warn=True)
-        if result.failed:
-            return False
-        result = run(f'rm /tmp/{file_name}', warn=True)
-        if result.failed:
-            return False
-        result = run('rm -f {current_path}', warn=True)
-        if result.failed:
-            return False
-        result = run(f'ln -sf {release_path} {current_path}', warn=True)
-        if result.failed:
-            return False
+    try:
+        file_name = os.path.basename(archive_path)
+        folder_name = file_name.split('.')[0]
+        release_path = f'/data/web_static/releases/{folder_name}'
+        tmp_path = f'/tmp/{file_name}'
+        current_path = f'/data/web_static/current'
+        for host in env.hosts:
+            put(archive_path, f'{tmp_path}')
+            run(f"rm -rf {release_path}/")
+            run(f"mkdir -p {release_path}/")
+            result = run(f'tar -xzvf {tmp_path} -C {release_path}/', warn=True)
+            if result.failed:
+                return False
+            result = run(f'rm {tmp_path}', warn=True)
+            if result.failed:
+                return False
+            run(f'mv /data/web_static/releases/{folder_name}/web_static/* {release_path}/')
+            run(f'rm -rf /data/web_static/releases/{folder_name}/web_static')
+            result = run('rm -rf {current_path}', warn=True)
+            if result.failed:
+                return False
+            result = run(f'ln -sf {release_path}/ {current_path}', warn=True)
+            if result.failed:
+                return False
+    except Exception:
+        return False
     return True
