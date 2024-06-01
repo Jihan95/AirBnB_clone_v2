5a5028a362be778ff7ed4d05010ede56107e1618
Jihan Ahmed <Jihanahmed41@gmail.com>
2024-06-01 13:40:01 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-""" Starts a Flask web app """
+""" app module """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ lists all State objects present in DBStorage sorted by name (A->Z)"""
+    states_dict = storage.all(State)
+    states = list(states_dict.values())
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def dispose(exception):
-    """ Remove current session """
+def close(e):
+    """ remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/states_list')
-def states():
-    """ Display list of all the states """
-    states = storage.all(State)
-    states_list = list(states.values())
-    return render_template('7-states_list.html', states=states_list)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
