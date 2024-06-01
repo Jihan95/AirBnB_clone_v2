1da95eb4efe72a18b177fbb96f8269d887d68bdb
Jihan Ahmed <Jihanahmed41@gmail.com>
2024-05-31 20:52:59 -0700

    updating file 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,5 +23,11 @@ def c(text):
     return f"c {text.replace('_', ' ')}"
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """ Custom 404 error page """
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
