846ec237eaa2d87f896f5d3cb7a5f1bdc4fbbd8f
Jihan Ahmed <Jihanahmed41@gmail.com>
2024-06-01 07:01:35 -0700

    update task 7

--- models/state.py
+++ models/state.py
@@ -6,6 +6,9 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.engine import file_storage
+from models.city import City
+import os
+storage = os.environ.get("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
@@ -16,11 +19,21 @@ class State(BaseModel, Base):
     name (str): state name
     """
 
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
+    __tablename__ = 'states'
+    if storage == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all,delete", backref="state")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        all_cities = file_storage.FileStorage.all(self)
-        return all_cities
+        @property
+        def cities(self):
+            """return the list of City objects from
+            storage linked to the current State"""
+            from models import storage
+            list_of_cities = []
+            allcities = storage.all(City)
+            for city in allcities.values():
+                if city.state_id == self.id:
+                    list_of_cities.append(city)
+            return list_of_cities
