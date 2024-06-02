25296760e586aedb7ceee042461aadb80c9a629d
Jihan Ahmed <Jihanahmed41@gmail.com>
2024-06-01 21:25:05 -0700

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""App module"""
+from flask import Flask, render_template
+import models
+from models.state import State
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """view that lists all of the states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_state_cities():
+    """view that lists all cities by their states"""
+    states = models.storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def close(error):
+    """remove the current SQLAlchemy Session"""
+    models.storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
