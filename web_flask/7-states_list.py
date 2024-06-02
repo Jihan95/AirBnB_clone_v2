c31b84de7951f38fe4a3e1ad04b94fdd228aba25
Jihan Ahmed <Jihanahmed41@gmail.com>
2024-06-01 20:31:18 -0700

    file 7.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,76 +1,15 @@
 #!/usr/bin/python3
 """ app module """
 from flask import Flask, abort, render_template
+from models import storage
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """ hello hbnb """
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """ hbnb """
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """ c is fun """
-    return f"C {text.replace('_', ' ')}"
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """ Python is cool """
-    return f"Python {text.replace('_', ' ')}"
-
-
-@app.route("/number/<n>", strict_slashes=False)
-def number(n):
-    """ display “n is a number” only if n is an integer """
-    try:
-        n = int(n)
-        return f"{n} is a number"
-    except ValueError:
-        abort(404)
-
-
-@app.route("/number_template/<n>", strict_slashes=False)
-def number_template(n):
-    """ display “n is a number” only if n is an integer """
-    try:
-        n = int(n)
-        return render_template('5-number.html', n=n)
-    except ValueError:
-        abort(404)
-
-
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
-def number_odd_or_even(n):
-    """ check if n is even or odd """
-    try:
-        n = int(n)
-        return render_template('6-number_odd_or_even.html', n=n)
-    except ValueError:
-        abort(404)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """ Custom 404 error page """
-    return (
-            '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n'''
-            '''<title>404 Not Found</title>\n'''
-            '''<h1>Not Found</h1>\n'''
-            '''<p>The requested URL was not found on the server. '''
-            '''If you entered the URL manually please check your spelling '''
-            '''and try again.</p>\n''', 404
-            )
+@app.teardown_appcontext
+def tear_down(error):
+    """remove the current SQLAlchemy Session"""
+    models.storage.close()
 
 
 if __name__ == "__main__":
