06ccb5ea96ec16f41568136fefdd18761c096e0c
Jihan Ahmed <Jihanahmed41@gmail.com>
2024-06-04 21:28:29 -0700

    task 9 issues

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ App module """
-from flask import Flask, render_template
-import models
-from models.state import State
 
 
-app = Flask(__name__)
+if __name__ == '__main__':
+    from flask import Flask, render_template
+    import models
+    from models.state import State
 
-def main():
+    app = Flask(__name__)
+
     """ main function """
     @app.route('/states_list', strict_slashes=False)
     def list_states():
@@ -15,20 +16,15 @@ def main():
         states = storage.all(State).values()
         return render_template('7-states_list.html', states=states)
 
-
     @app.route('/cities_by_states', strict_slashes=False)
     def list_state_cities():
         """ view that lists all cities by their states """
         states = models.storage.all(State).values()
         return render_template('8-cities_by_states.html', states=states)
 
-
     @app.teardown_appcontext
     def close(error):
         """ remove the current SQLAlchemy Session """
         models.storage.close()
 
-
-if __name__ == '__main__':
-    main()
     app.run(host='0.0.0.0', port=5000)
